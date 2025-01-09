dcc3e7b77b0a657cf34d6e4a9acd4813743e6ce9
Gotyz3 <znxfenix@gmail.com>
2025-01-09 06:02:49 -0300

    Update ScriptHubV1 09.01.2025.lua

--- ScriptHubV1 09.01.2025.lua
+++ ScriptHubV1 09.01.2025.lua
@@ -7,119 +7,110 @@ local Window = OrionLib:MakeWindow({
     SaveConfig = true,
     ConfigFolder = "OrionTest"
 })
---tab do blox fruits
+
+-- Tab: Anime Shadow
 local Tab1 = Window:MakeTab({
-    Name = "Blox Fruits",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
-Tab1:AddButton({
-    Name = "Hoho Hub",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI", true))()
-    end    
-})
-Tab1:AddButton({
-    Name = "OP GUI",
-    Callback = function()
-        loadstring(game:HttpGet("https://nicuse.xyz/MainHub.lua"))()
-    end    
-})
-
-Tab1:AddButton({
-    Name = "Dead Hub",
-    Callback = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Deadhub0/Blox-fruits/refs/heads/main/Script'))() 
-    end    
-})
-
-Tab1:AddButton({
-    Name = "Solix Hub",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/Solixreworkkeysystem/refs/heads/main/solix%20new%20keyui.lua"))()
-    end    
-})
-
-Tab1:AddButton({
-    Name = "Relz Hub",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/farghii/relzhub/main/execute.hack", true))()
-    end    
-})
-
---Blue lock Rivals
-
-local Tab2 = Window:MakeTab({
-    Name = "Blue Lock Rivals",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
-Tab2:AddButton({
-    Name = "NS HUB",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Mc4121ban/RobloxScirpt/refs/heads/main/ScriptHub"))()
-    end    
-})
-
-Tab2:AddButton({
-    Name = "Rinn Hub",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/SkibidiCen/MainMenu/main/Code"))()
-    end    
-})
-
---Tab Rivals
-
-local Tab3 = Window:MakeTab({
-    Name = "Rivals",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
-Tab3:AddButton({
-    Name = "X8 bit",
-    Callback = function()
-        loadstring(game:HttpGet("https://x8-bit.web.app/scripts/rival_c2.lua", true))()
-    end    
-})
-
-Tab3:AddButton({
-    Name = "?",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Exlarelol/test1/refs/heads/main/sex", true))()
-    end    
-})
-
---Anime Shadow
-
-local Tab4 = Window:MakeTab({
     Name = "Anime Shadow",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 
-Tab4:AddButton({
+Tab1:AddButton({
     Name = "ZardoX",
     Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI", true))()
     end    
 })
 
---Jujutsu Infinite
-
-local Tab5 = Window:MakeTab({
-    Name = "Jujutsu Infinite",
+-- Tab: Blox Fruits
+local Tab2 = Window:MakeTab({
+    Name = "Blox Fruits",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 
-Tab5:AddButton({
+Tab2:AddButton({
+    Name = "Dead Hub",
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Deadhub0/Blox-fruits/refs/heads/main/Script'))() 
+    end    
+})
+
+Tab2:AddButton({
+    Name = "Hoho Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI", true))()
+    end    
+})
+
+Tab2:AddButton({
+    Name = "OP GUI",
+    Callback = function()
+        loadstring(game:HttpGet("https://nicuse.xyz/MainHub.lua"))()
+    end    
+})
+
+Tab2:AddButton({
+    Name = "Relz Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/farghii/relzhub/main/execute.hack", true))()
+    end    
+})
+
+Tab2:AddButton({
+    Name = "Solix Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/Solixreworkkeysystem/refs/heads/main/solix%20new%20keyui.lua"))()
+    end    
+})
+
+-- Tab: Blue Lock Rivals
+local Tab3 = Window:MakeTab({
+    Name = "Blue Lock Rivals",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+Tab3:AddButton({
     Name = "NS HUB",
     Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/Infinite/refs/heads/main/All"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Mc4121ban/RobloxScirpt/refs/heads/main/ScriptHub"))()
     end    
+})
+
+Tab3:AddButton({
+    Name = "Rinn Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/SkibidiCen/MainMenu/main/Code"))()
+    end    
+})
+
+-- Tab: FISCH
+local Tab4 = Window:MakeTab({
+    Name = "FISCH",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+Tab4:AddButton({
+    Name = "Forge Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Skzuppy/forge-hub/main/loader.lua"))() 
+    end    
+})
+
+Tab4:AddButton({
+    Name = "OP HUB",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/kiciahook/kiciahook/refs/heads/main/loader.lua"))()
+    end    
+})
+
+-- Tab: Jujutsu Infinite
+local Tab5 = Window:MakeTab({
+    Name = "Jujutsu Infinite",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 Tab5:AddButton({
@@ -129,8 +120,14 @@ Tab5:AddButton({
     end    
 })
 
---Murder Mystery
+Tab5:AddButton({
+    Name = "NS HUB",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/Infinite/refs/heads/main/All"))()
+    end    
+})
 
+-- Tab: Murder Mystery
 local Tab6 = Window:MakeTab({
     Name = "Murder Mystery",
     Icon = "rbxassetid://4483345998",
@@ -144,26 +141,26 @@ Tab6:AddButton({
     end    
 })
 
---FISCH
-
+-- Tab: Rivals
 local Tab7 = Window:MakeTab({
-    Name = "FISCH",
+    Name = "Rivals",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 
 Tab7:AddButton({
-    Name = "Forge Hub",
+    Name = "?",
     Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Skzuppy/forge-hub/main/loader.lua"))() 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Exlarelol/test1/refs/heads/main/sex", true))()
     end    
 })
 
 Tab7:AddButton({
-    Name = "OP HUB",
+    Name = "X8 bit",
     Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/kiciahook/kiciahook/refs/heads/main/loader.lua"))()
+        loadstring(game:HttpGet("https://x8-bit.web.app/scripts/rival_c2.lua", true))()
     end    
 })
 
+-- Inicializa o OrionLib
 OrionLib:Init()
